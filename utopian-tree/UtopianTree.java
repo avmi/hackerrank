26fd273f9d502f7303511d9c7afd1b0a65d211e9
Andrey Miroshnichenko <a@avm.name>
2016-01-16 14:09:55 +0300

    Utopian Tree

--- utopian-tree/UtopianTree.java
+++ utopian-tree/UtopianTree.java
@@ -1,15 +1,34 @@
 import java.io.*;
 import java.util.*;
-
-// TODO
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class UtopianTree {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
-            int n = in.nextInt();
+    public static int solve(int T, boolean first, int size) {
+        if (T==0) {
+            return size;
+        }
+
+        if (!first) {
+            return solve(T - 1, true, size + 1);
+        }
+        else {
+            return solve(T - 1, false, size * 2);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inp;
+        int cases = Integer.parseInt(inp = br.readLine());
+
+        for(int i=0; i < cases; i++) {
+            int year = Integer.parseInt(inp = br.readLine());
+            int sol = solve(year,true, 1);
+            System.out.println(sol);
         }
     }
 }
