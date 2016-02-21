70e069b24a4d9ca3c01d65b594b666b256e60ae4
Andrey Miroshnichenko <a@avm.name>
2016-02-21 22:33:25 +0300

    String Similarity rewrite

--- string-similarity/StringSimilarity.java
+++ string-similarity/StringSimilarity.java
@@ -1,36 +1,64 @@
-import java.io.*;
 import java.util.*;
-
-// TODO: Fix timeout in test cases #10 and #11
 
 public class StringSimilarity {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int numCases = scanner.nextInt();
+    static long stringSimilarity(String a) {
 
-        for (int i = 0; i < numCases; i++) {
-            String input = scanner.next();
-            System.out.println(solve(input));
+        long sum = a.length();
+
+        int[] z = calc(a);
+        for (int i = 1; i < a.length(); i++) {
+            sum = sum + z[i];
         }
+
+        return sum;
     }
 
-    public static int solve(String s) {
-        int length = s.length();
-        int count = length;
+    static int[] calc(String text) {
+        int len = text.length();
+        int[] res = new int[len];
 
-        for (int i = 1; i < length; i++) {
-            int len = length - i;
-            int j;
+        int L = 0, R = 0;
+        for (int i = 1; i < len; i++) {
+            if (i > R) {
+                L = R = i;
 
-            for(j = 0; j < len; j++) {
-                if (s.charAt(j) != s.charAt(j + i)) {
-                    break;
+                while (R < len && text.charAt(R - L) == text.charAt(R)) {
+                    R++;
+                }
+
+                res[i] = --R - L + 1;
+            } else {
+                int k = i - L;
+
+                if (res[k] < R - i + 1) {
+                    res[i] = res[k];
+                } else {
+                    L = i;
+
+                    while (R < len && text.charAt(R - L) == text.charAt(R)) {
+                        R++;
+                    }
+
+                    res[i] = --R - L + 1;
                 }
             }
-            count += j;
         }
 
-        return count;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int T = Integer.parseInt(in.nextLine());
+
+        for (int i = 0; i < T; i++) {
+            String _a = in.nextLine();
+
+            long res = stringSimilarity(_a);
+
+            System.out.println(res);
+        }
     }
 }
