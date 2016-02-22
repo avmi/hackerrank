cb5de81b03664002a5980d00bf68811a1391021e
Andrey Miroshnichenko <a@avm.name>
2016-02-22 10:20:38 +0300

    Service Lane

---
+++ service-lane/ServiceLane.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class ServiceLane {
+    public static void main(String[] args) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] temp = br.readLine().split(" ");
+        int N = Integer.parseInt(temp[0]);
+        short T = Short.parseShort(temp[1]);
+        byte[] width = new byte[N];
+        temp = br.readLine().split(" ");
+
+        for (int i = 0; i < N; ++i) {
+            width[i] = Byte.parseByte(temp[i]);
+        }
+
+        while (T-- > 0) {
+            temp = br.readLine().split(" ");
+
+            int i = Integer.parseInt(temp[0]);
+            int j = Integer.parseInt(temp[1]);
+            int min = width[i];
+
+            while (min > 1 && i++ < j) {
+                min = min < width[i] ? min : width[i];
+            }
+
+            sb.append(min + "\n");
+        }
+
+        System.out.print(sb);
+    }
+}
